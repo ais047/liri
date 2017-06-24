74ae5e501c4525e3bd6fd21d38b3c636ac712f58
ais047 <hursungwoo@yahoo.com>
2017-06-23 20:43:26 -0700

    done

--- liri.js
+++ liri.js
@@ -1,8 +1,12 @@
 var Twitter = require('twitter');
 var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
+var fs = require('fs');
+var request = require('request');
 
-
+var select;
+var selection = process.argv[2];
+var query = process.argv[3];
 var access_key = keys.twitterKeys.consumer_key;
 var access_secret = keys.twitterKeys.consumer_secret;
 var token_key = keys.twitterKeys.access_token_key;
@@ -30,33 +34,88 @@ var spotify = new Spotify({
 	secret: client_secret
 });
 
+function runtime(){
+
+if(selection == "my-tweets" || selection == 1){
+	select = 1;
+}
+else if(selection == "spotify-this-song" || selection == 2){
+	select = 2;
+}
+	
+else if(selection == "movie-this" || selection == 3){
+	select = 3;
+}
+	
+else if(selection == "do-what-it-says" || selection == 4){
+	select = 4;
+}
+
+switch(select){
+	case 1: tweet();
+		break;
+	case 2: spot();
+		break;
+	case 3: movie();
+		break;
+	case 4: doit();
+		break;
+	default:
+		return console.log("??");
+	}
+}
+runtime();
 
 
 function tweet(){
-	client.get('statuses/user_timeline', params, function(error, tweets, response){
+	client.get('statuses/user_timeline/', params, function(error, tweets, response){
 		if(!error){
-			console.log(tweets);	
+			for(var i = 0; i < tweets.length; i++){
+				console.log(tweets[i].text);
+				console.log("created at:" + tweets[i].created_at);
+			}
 		}
 	});
 }
 
 
-function spotify(){
-	var song = process.argv[3];
+function spot(){
 	spotify.search({
-		type:'track', query: 'Lazenca', function(error, data){
+		type:'track', query: query}, function(error, data){
 		if(error){
 			return console.log(error);
 		}else{
-			console.log(data);
+			//console.log(data.tracks.items[0]);
+			console.log("Name: " + data.tracks.items[0].name);
+			console.log("Artist: " + data.tracks.items[0].artists.name);
+			console.log("Album: " + data.tracks.items[0].album.name);
+			console.log("Preview URL: " + data.tracks.items[0].preview_url);
 		}
-		}
+		
 	})
 }
 
 
 function movie(){
+	request('http://www.omdbapi.com/?apikey=40e9cece&s=' + query,  function(error,response, body){
+		var thing = JSON.parse(body);
+		console.log(thing.Search[0]);
+	})
 }
 function doit(){
+	var temp = [];
+	fs.readFile('random.txt', 'utf8', function(err,data){
+		if(err){
+			console.log(err);
+		}else{
+			console.log(data);
+			temp = data.split(',');
+			console.log(temp[0], temp[1]);
+			selection = temp[0];
+			query = temp[1];	
+			runtime();
+		}
+	});
 }
+
 
